d810ec5d58d32417c82b16278266f980abb88e9b
Ringo Hoffmann <riho@live.de>
2021-02-26 15:48:19 +0100

    update version

--- src/main/java/de/zekro/log4forge/Log4Forge.java
+++ src/main/java/de/zekro/log4forge/Log4Forge.java
@@ -27,7 +27,7 @@ import java.util.List;
 public class Log4Forge {
     public static final String MODID = "log4forge";
     public static final String NAME = "Log4Forge";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "0.1.0";
 
     private static Logger logger;
 
